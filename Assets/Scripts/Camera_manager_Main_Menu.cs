711a933429bb236ed2705f5a05475089f8aa3921
JoeY34kaze <ragnar1724@gmail.com>
2016-04-09 20:07:44 +0200

    EXTERMINATUS
    The unworthy have perished, their brood feeds the ground for new
    beginnings.

--- Assets/Scripts/Camera_manager_Main_Menu.cs
+++ Assets/Scripts/Camera_manager_Main_Menu.cs
@@ -6,7 +6,7 @@ public class Camera_manager_Main_Menu : MonoBehaviour {
 	public float speed=1.0f;
 	// Use this for initialization
 	void Start () {
-	
+		//GameObject.Find ("Network_manager").SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -20,13 +20,18 @@ public class Camera_manager_Main_Menu : MonoBehaviour {
 				// the object identified by hit.transform was clicked
 			//	Debug.Log ("raycast hit something");
 			//	Debug.Log (hit.transform.name);
-				if (hit.transform.name.Equals("Cube_play")) {
-			//		Debug.Log ("PLAY");
-					SceneManager.LoadScene("Test_scene");
+				if (hit.transform.name.Equals ("Cube_play")) {
+					//		Debug.Log ("PLAY");
+					SceneManager.LoadScene ("Test_scene");
 					
 				} else if (hit.transform.name.Equals ("Cube_exit")) {
-			//		Debug.Log ("QUIT");
-					Application.Quit();
+					//		Debug.Log ("QUIT");
+					Application.Quit ();
+				} else if (hit.transform.name.Equals ("Sphere_multiplayer")) {
+					//nrdit karkol je pac treba za multiplayer
+					//od network managerja treba pokazat HUD tle
+					//GameObject.Find ("Network_manager").SetActive(true);
+
 				}
 			} else {
 				Debug.Log("-\n");
---
+++ Assets/Scripts/Player_move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
