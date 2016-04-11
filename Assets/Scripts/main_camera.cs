11059c86e18a067a3537a488f5c76d567d09e0c9
JoeY34kaze <ragnar1724@gmail.com>
2016-04-12 00:39:06 +0200

    hmmmm
    hmmmm
    samo na hostu dela tko k mora
    mogoce rpc al pa kej nevem

--- Assets/Scripts/main_camera.cs
+++ Assets/Scripts/main_camera.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class main_camera : NetworkBehaviour {
-	override public void OnStartLocalPlayer(){
+	override public void OnStartLocalPlayer(){//ce zamenam na onnetworkinstatiate follow ne dela. tole mislm da nrdi samo hostu, ne izvede se pa na clientih. mogoče rpc nrdit al pa kej?
 #if UNITY_EDITOR
 		Camera.main.GetComponent<SmoothFollow> ().target = transform;
 #endif
