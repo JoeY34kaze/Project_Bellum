9fa87d80e072774963900197fe94dfd0a8d7e028
JoeY34kaze <ragnar1724@gmail.com>
2016-04-17 22:27:06 +0200

    networkAnimator+++
    networkanimator
    paladin
    walk/idle animation
    charactercontroller

--- Assets/Scripts/player_camera_controller.cs
+++ Assets/Scripts/player_camera_controller.cs
@@ -8,7 +8,17 @@ public class player_camera_controller : NetworkBehaviour {
 		Camera.main.transform.SetParent(transform);//nastav kamero kot child object
 		Camera.main.GetComponent<SmoothFollow> ().target = transform;
 
+		//Renderer[] rends = GetComponentsInChildren<Renderer> ();//tole za renderje je blo u tutorialu https://www.youtube.com/watch?v=ZsZiHe8yN9A ampak nevem zakaj
+		//foreach (Renderer r in rends) {
+		//	r.enabled=false;
+		//}
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (0, true);//tole je nastavlen sam na local player, treba je še na remote
 //#endif
 	}
 
+	public override void PreStartClient ()
+	{
+		//base.PreStartClient ();
+		GetComponent<NetworkAnimator> ().SetParameterAutoSend (0, true);//zdj je še na remote
+	}
 }
