ee172973fcf55481cc25a55a322f8f93e48d0d2e
JoeY34kaze <ragnar1724@gmail.com>
2016-04-18 21:22:10 +0200

    mal vec animacij
    asdasd

--- Assets/Scripts/charcater_controller.cs
+++ Assets/Scripts/charcater_controller.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class charcater_controller : NetworkBehaviour {
-	public float speed = 6.0F;
+	public float speed = 1.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+	public float run=1.5f;
+	public float sprint=1.5f;
+	private float horizontal = 0;
 	private Vector3 moveDirection = Vector3.zero;
 	private Animator anim;
 	void Start(){
@@ -20,11 +23,34 @@ public class charcater_controller : NetworkBehaviour {
 
 		CharacterController controller = GetComponent<CharacterController>();
 		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			horizontal = Input.GetAxis ("Horizontal");
+			anim.SetFloat ("horizontal",Input.GetAxis("Horizontal"));
+			moveDirection = new Vector3(horizontal, 0, Input.GetAxis("Vertical"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
 
 			anim.SetFloat ("movement_speed",Input.GetAxis ("Vertical"));
+
+			if (!Input.GetButton ("Run") && !Input.GetButton ("Modifier1")) {
+				anim.SetBool ("Sprinting", false);
+				anim.SetBool ("Running", false);
+			}
+
+			if (Input.GetButton ("Run")) {
+				anim.SetBool ("Running", true);
+				moveDirection *= run;
+				if (Input.GetButton ("Modifier1")) {
+					anim.SetBool ("Sprinting", true);
+					moveDirection *= sprint;
+				} else {
+					anim.SetBool ("Sprinting", false);
+				}
+			} else {
+				anim.SetBool ("Running", false);
+			}
+
+
+
 
 			if (Input.GetButton("Jump"))
 				moveDirection.y = jumpSpeed;
